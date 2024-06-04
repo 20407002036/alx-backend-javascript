b2febca717a25277a33aecc0183d3564d6d42d0f
Kyo <solomonkaniaru154@gmail.com>
2024-06-04 19:01:42 +0300

    commit for the student.id

--- 0x03-ES6_data_manipulation/1-main.js
+++ 0x03-ES6_data_manipulation/1-main.js
@@ -1,3 +1,9 @@
+/**
+ * Gets the ids of students if the args is of type array
+ * @author Solomon Kaniaru
+ * @returns list of ids
+ */
+
 import getListStudentIds from "./1-get_list_student_ids.js";
 import getListStudents from "./0-get_list_students.js";
 
